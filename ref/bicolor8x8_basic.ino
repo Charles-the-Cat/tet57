56f5c22d2a78811d1fcfe818d7826d8617dc9b66
Charles <depalman17@gmail.com>
2019-02-11 16:23:18 -0500

    brought over 8x8 example code

---
+++ ref/bicolor8x8_basic.ino
@@ -0,0 +1,109 @@
+/*
+	I2C LED Backpack for 8x8 bicolor matrix
+
+	NOTE: using the Arduino UNO/ Metro Mini with this bicolor8x8, 
+	Adafruit neglects to tell us the following:
+ 
+	Data  -> pin A4
+	Clock -> pin A5
+*/
+
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include "Adafruit_LEDBackpack.h"
+
+Adafruit_BicolorMatrix matrix = Adafruit_BicolorMatrix();
+
+void setup() {
+  
+  matrix.begin(0x70);  // pass in the address
+}
+
+static const uint8_t PROGMEM
+  smile_bmp[] =
+  { B00111100,
+    B01000010,
+    B10100101,
+    B10000001,
+    B10100101,
+    B10011001,
+    B01000010,
+    B00111100 },
+  neutral_bmp[] =
+  { B00111100,
+    B01000010,
+    B10100101,
+    B10000001,
+    B10111101,
+    B10000001,
+    B01000010,
+    B00111100 },
+  frown_bmp[] =
+  { B00111100,
+    B01000010,
+    B10100101,
+    B10000001,
+    B10011001,
+    B10100101,
+    B01000010,
+    B00111100 };
+
+void loop() {
+
+  matrix.clear();
+  matrix.drawBitmap(0, 0, smile_bmp, 8, 8, LED_GREEN);
+  matrix.writeDisplay();
+  delay(500);
+
+  matrix.clear();
+  matrix.drawBitmap(0, 0, neutral_bmp, 8, 8, LED_YELLOW);
+  matrix.writeDisplay();
+  delay(500);
+
+  matrix.clear();
+  matrix.drawBitmap(0, 0, frown_bmp, 8, 8, LED_RED);
+  matrix.writeDisplay();
+  delay(500);
+
+  matrix.clear();      // clear display
+  matrix.drawPixel(0, 0, LED_GREEN);  
+  matrix.writeDisplay();  // write the changes we just made to the display
+  delay(500);
+
+  matrix.clear();
+  matrix.drawLine(0,0, 7,7, LED_YELLOW);
+  matrix.writeDisplay();  // write the changes we just made to the display
+  delay(500);
+
+  matrix.clear();
+  matrix.drawRect(0,0, 8,8, LED_RED);
+  matrix.fillRect(2,2, 4,4, LED_GREEN);
+  matrix.writeDisplay();  // write the changes we just made to the display
+  delay(500);
+
+  matrix.clear();
+  matrix.drawCircle(3,3, 3, LED_YELLOW);
+  matrix.writeDisplay();  // write the changes we just made to the display
+  delay(500);
+
+  matrix.setTextWrap(false);  // we dont want text to wrap so it scrolls nicely
+  matrix.setTextSize(1);
+  matrix.setTextColor(LED_GREEN);
+  for (int8_t x=7; x>=-36; x--) {
+    matrix.clear();
+    matrix.setCursor(x,0);
+    matrix.print("Hello");
+    matrix.writeDisplay();
+    delay(100);
+  }
+  matrix.setRotation(3);
+  matrix.setTextColor(LED_RED);
+  for (int8_t x=7; x>=-36; x--) {
+    matrix.clear();
+    matrix.setCursor(x,0);
+    matrix.print("World");
+    matrix.writeDisplay();
+    delay(100);
+  }
+  matrix.setRotation(0);
+}
