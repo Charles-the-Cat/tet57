4ccc770dc390ca119330de8e47de821216614fb9
Charles <depalman17@gmail.com>
2019-02-12 09:24:15 -0500

    Began integrating with bicolor8x8, added macros, etc.

--- tet.ino
+++ tet.ino
@@ -32,8 +32,12 @@
 #define VWIDTH 5
 #define VHEIGHT 7
 
-static byte dbuf[WIDTH*HEIGHT] = { OFF };
-static byte  buf[WIDTH*HEIGHT] = 
+// macros to convert linear array into X and Y coords
+#define buf2x( k ) ((k)%(WIDTH))
+#define buf2y( k ) ((k)/(WIDTH))
+
+static unsigned byte dbuf[WIDTH*HEIGHT] = { OFF };
+static unsigned byte  buf[WIDTH*HEIGHT] = 
 {
 	ON,0,0,0,0,0,ON,
 	ON,0,0,0,0,0,ON,
@@ -71,11 +75,44 @@ void newShape()
 bool isGameOver()
 {
 	// check if the top row, excluding the first and last, has any ON
-	for ( byte i = 1; i < WIDTH - 1; i++ )
+	for ( unsigned byte i = 1; i < WIDTH - 1; i++ )
 		if ( i = ON ) return true;
 	return false;
 }
 
-void setup() {}
-void loop()  {}
+/* Implementation stuff follows */
+
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include "Adafruit_LEDBackpack.h"
+Adafruit_BicolorMatrix matrix = Adafruit_BicolorMatrix();
+void setup() {matrix.begin(0x70);}
+void loop()
+{
+	matrix.clear();
+	for ( unsigned byte i = 0; i < WIDTH * HEIGHT; i++ )
+	{
+		if ( buf[i] || dbuf[i] )
+		{
+			matrix.drawPixel( buf2x(i), buf2y(i), LED_YELLOW );
+		}
+	}
+	matrix.writeDisplay();
+	delay(100);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
