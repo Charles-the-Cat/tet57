64eb02c5c6190b5c47927e342292b405b43896a7
Charles <depalman17@gmail.com>
2019-02-12 09:38:56 -0500

    renamed tet to tet57 because arduino, fixed macro name collision with adafruit header...?

---
+++ tet57.ino
@@ -0,0 +1,117 @@
+/*
+   ()()()()()()()
+   ()[][][][][]()
+   ()[][][][][]()
+   ()[][][][][]()
+   ()[][][][][]()
+   ()[][][][][]()
+   ()[][][][][]()
+   ()[][][][][]()
+   ()()()()()()()
+
+   [][][]
+
+     []
+   [][]
+
+     []
+   [][][]
+
+   [][]
+   [][]
+*/
+
+#define OFF 0
+#define ON ~0
+
+// buffer width and height
+#define TET_WIDTH 7
+#define TET_HEIGHT 9
+// visible buffer width and height
+#define TET_VWIDTH 5
+#define TET_VHEIGHT 7
+
+// macros to convert linear array into X and Y coords
+#define buf2x( k ) ((k)%(TET_WIDTH))
+#define buf2y( k ) ((k)/(TET_WIDTH))
+
+static unsigned byte dbuf[TET_WIDTH*TET_HEIGHT] = { OFF };
+static unsigned byte  buf[TET_WIDTH*TET_HEIGHT] = 
+{
+	ON,0,0,0,0,0,ON,
+	ON,0,0,0,0,0,ON,
+	ON,0,0,0,0,0,ON,
+	ON,0,0,0,0,0,ON,
+	ON,0,0,0,0,0,ON,
+	ON,0,0,0,0,0,ON,
+	ON,0,0,0,0,0,ON,
+	ON,0,0,0,0,0,ON,
+	ON,ON,ON,ON,ON,ON,ON
+};
+
+void newShape()
+{
+	// first, clear dbuf
+	memset( dbuf, OFF, TET_WIDTH*TET_HEIGHT*sizeof(byte) );
+	switch( random(4) )
+	{
+		default:
+		case 0: // ...
+			dbuf[2]=ON;dbuf[3]=ON;dbuf[4]=ON;
+			break;
+		case 1: // .:
+			dbuf[2]=ON;dbuf[3]=ON;dbuf[10]=ON;
+			break;
+		case 2: // .:.
+			dbuf[2]=ON;dbuf[3]=ON;dbuf[4]=ON;dbuf[10]=ON;
+			break;
+		case 3: // ::
+			dbuf[2]=ON;dbuf[3]=ON;dbuf[9]=ON;dbuf[10]=ON;
+			break;
+	}
+}
+
+bool isGameOver()
+{
+	// check if the top row, excluding the first and last, has any ON
+	for ( unsigned byte i = 1; i < TET_WIDTH - 1; i++ )
+		if ( i = ON ) return true;
+	return false;
+}
+
+/* Implementation stuff follows */
+
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include "Adafruit_LEDBackpack.h"
+Adafruit_BicolorMatrix matrix = Adafruit_BicolorMatrix();
+void setup() {matrix.begin(0x70);}
+void loop()
+{
+	matrix.clear();
+	for ( unsigned byte i = 0; i < TET_WIDTH * TET_HEIGHT; i++ )
+	{
+		if ( buf[i] || dbuf[i] )
+		{
+			matrix.drawPixel( buf2x(i), buf2y(i), LED_YELLOW );
+		}
+	}
+	matrix.writeDisplay();
+	delay(100);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
