a5525e58181c41b40692f2106ba9725d04b038db
Charles <depalman17@gmail.com>
2019-02-14 09:32:57 -0500

    we may finally see shapes!

--- tet57.ino
+++ tet57.ino
@@ -32,8 +32,10 @@
 #define TET_VHEIGHT 7
 
 // macros to convert linear array into X and Y coords
-#define buf2x( k ) ((k)%(TET_WIDTH))
-#define buf2y( k ) ((k)/(TET_WIDTH))
+#define  buf2x( k ) ((k)%(TET_WIDTH))
+#define  buf2y( k ) ((k)/(TET_WIDTH))
+#define vbuf2x( k ) // TODO
+#define vbuf2y( k )
 
 static uint8_t dbuf[TET_WIDTH*TET_HEIGHT] = { OFF };
 static uint8_t  buf[TET_WIDTH*TET_HEIGHT] = 
@@ -51,8 +53,6 @@ static uint8_t  buf[TET_WIDTH*TET_HEIGHT] =
 
 void newShape()
 {
-	// first, clear dbuf
-	memset( dbuf, OFF, TET_WIDTH*TET_HEIGHT*sizeof(uint8_t) );
 	switch( random(4) )
 	{
 		default:
@@ -71,12 +71,35 @@ void newShape()
 	}
 }
 
-bool isGameOver()
+void dbufFreeze()
 {
-	// check if the top row, excluding the first and last, has any ON
-	for ( uint8_t i = 1; i < TET_WIDTH - 1; i++ )
-		if ( i = ON ) return true;
-	return false;
+	// OR dbuf into buf
+	for ( uint8_t i = 0; i < TET_WIDTH*TET_HEIGHT; i++ )
+	{
+		buf[i] |= dbuf[i];
+	}
+	// clear dbuf, ready for the next shape
+	memset( dbuf, OFF, TET_WIDTH*TET_HEIGHT*sizeof(uint8_t) );
+}
+
+void dbufAdvance()
+{
+	for ( uint8_t i = TET_WIDTH*(TET_HEIGHT-1)-1; i >= 0; i-- )
+	{
+		if ( dbuf[i] )
+		{
+			if ( !buf[i+TET_WIDTH] ) // the space directly below is clear
+			{
+				dbuf[i] = OFF;
+				dbuf[i+TET_WIDTH] = ON; // move the block down one row
+			}
+			else
+			{
+				dbufFreeze();
+				newShape();
+			}
+		}
+	}
 }
 
 /* Implementation stuff follows */
@@ -98,6 +121,7 @@ void loop()
 	}
 	matrix.writeDisplay();
 	delay(100);
+	dbufAdvance();
 }
 
 
