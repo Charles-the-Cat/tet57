957c884381936e514f5cefdfe921fc56d89c2dc0
Charles <depalman17@gmail.com>
2019-02-15 12:56:21 -0500

    created separate interrupt system to replace setup() and loop()

--- tet57.ino
+++ tet57.ino
@@ -38,7 +38,7 @@
 #define  buf2x( k ) ((k)%(TET_WIDTH))
 #define  buf2y( k ) ((k)/(TET_WIDTH))
 #define vbuf2x( k ) // TODO
-#define vbuf2y( k )
+#define vbuf2y( k ) // TODO
 
 static uint8_t dbuf[TET_WIDTH*TET_HEIGHT] = { OFF };
 static uint8_t  buf[TET_WIDTH*TET_HEIGHT] = 
@@ -111,6 +111,9 @@ void dbufAdvance()
 #include <Adafruit_GFX.h>
 #include "Adafruit_LEDBackpack.h"
 Adafruit_BicolorMatrix matrix = Adafruit_BicolorMatrix();
+
+/* Arduino-style setup() and loop() being used for now */
+#if 1
 void setup()
 {
 	matrix.begin(0x70);
@@ -132,8 +135,51 @@ void loop()
 	dbufAdvance();
 }
 
+/* AVR/timer code that will eventually replace the above setup() and loop() */
+#endif
+#if 1-1
+#include <avr/interrupt.h>
 
+void interruptSetup()
+{
+	cli(); // stop interrupts
 
+	TCCR1A = 0; // zero TCCR1A register
+	TCCR2A = 0; // zero TCCR2B register
+	TCNT1 = 0;  // initialize counter to 0
+	
+	OCR1A = 64000; // set compare match register for 250 Hz increments
+	// 64000 = 16000000 / (1 * 250 - 1)
+
+	TCCR1B |= ( 1 << WGM12 ); // turn on CTC
+	TCCR1B |= ( 0 << CS12 ) | ( 0 << CS11 ) | ( 1 << CS10 ); // set bits for x1 prescaler
+	TIMSK1 |= ( 1 << OCIE1A ); // enable timer compare interrupt
+
+	sei(); // allow interrupts
+}
+
+void main()
+{
+	matrix.begin( 0x70 );
+	randomSeed( analogRead( 0 ) );
+	newShape();
+	interruptSetup();
+}
+
+ISR ( TIMER1_COMPA_vect )
+{
+	dbufAdvance();
+	matrix.clear();
+	for ( uint8_t i = 0; i < TET_WIDTH * TET_HEIGHT; i++ )
+	{
+		if ( buf[i] || dbuf[i] )
+		{
+			matrix.drawPixel( buf2x(i), buf2y(i), LED_YELLOW );
+		}
+	}
+	matrix.writeDisplay();
+}
+#endif
 
 
 
